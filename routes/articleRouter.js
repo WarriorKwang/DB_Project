1ba95aef8a2c4ae6cb54c0e2b97eded801135480
KwangJin <test@naver.com>
2022-11-17 01:36:53 +0900

    test

--- routes/articleRouter.js
+++ routes/articleRouter.js
@@ -5,9 +5,13 @@ const articleListController = require('../controllers/articleListController');
 const articleReadController = require('../controllers/articleReadController');
 const articleWriteController = require('../controllers/articleWriteController');
 
-router.get('/page', articleListController.getArticleFirstPage);
-router.get('/page/:page', articleListController.getArticleList);
-router.get('/:page',articleReadController.getArticlePage);
-router.get('/write', articleWriteController.writeArticlePost); // 동작 확인 해봐야함
-router.get('/modify/:page');
+router.get('/list', articleListController.getArticleFirstPage);
+router.get('/list/:page', articleListController.getArticleList);
+router.get('/:article_idx', articleReadController.getArticlePage);
+
+// router.get('/page', articleListController.getArticleFirstPage);
+// router.get('/page/:page', articleListController.getArticleList);
+// router.get('/:page',articleReadController.getArticlePage);
+// router.get('/write', articleWriteController.writeArticlePost); // 동작 확인 해봐야함
+// router.get('/modify/:page');
 module.exports = router;
