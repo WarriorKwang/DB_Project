0c7f47ad6549d12fa5eebc30520464245e852005
MS5682 <mss5682@naver.com>
2022-12-11 20:29:56 +0900

    Update stockController.js

--- DB_PROJECT/controllers/stockController.js
+++ DB_PROJECT/controllers/stockController.js
@@ -56,9 +56,30 @@ exports.tradeStock = async (req, res, next)=>{
         let order_price = req.body.order_price;
         let OrderData = [user_id, stock_code, order_amount, order_price, order_amount]
         if(trade == 'sell')   //sell
+        {
+            let existedSellUserstock = await stockModel.getSellUserStock(user_id, stock_code); //주식 보유 수 가져옴 
+            if(existedSellUserstock.hold_amount < order_amount) //가지고있는 주식보다 주문량이 더 많음
+                return res.status(400).send('Bad Request');
+            else
+            { 
             await stockModel.sellOrder(OrderData);  //판매 요청
+            if(existedSellUserstock[0].hold_amount == order_amount) 
+                await stockModel.deleteSellUserStock(user_id, stock_code); //보유 수와 판매 수가 같으면 삭제
+            else
+                await stockModel.updateSellUserStock(order_amount, existedSellUserstock[0].all_price_bought,
+                         existedSellUserstock[0].hold_amount, user_id, stock_code ); //다르면 변경
+            }
+        }
         else if(trade == 'buy')       //buy
-            await stockModel.buyOrder(OrderData); //구매 요청
+        {
+            let user_money = await stockModel.getUserMoney(user_id);
+            if(user_money < order_price* order_amount)  //가지고 있는 돈보다 구매하는 금액이 더 큼
+                return res.status(400).send('Bad Request');
+            else{
+                await stockModel.updateBuyUserMoney(order_amount*order_price, user_id);  //유저가 구매시 보유 자산 감소
+                await stockModel.buyOrder(OrderData); //구매 요청
+            }
+        }
         await stockModel.beginTransaction();
         for(var i=0; i<req.body.order_amount; i++) //요청한 주문이 매칭되면 처리
         {
@@ -74,26 +95,16 @@ exports.tradeStock = async (req, res, next)=>{
                 let unconfirmed_buy_amount = matchTrade[0].buy_amount - confirmed_amount;  //미체결 구매 수량 = 구매 수량 - 체결 수량 
                 let unconfirmed_sell_amount = matchTrade[0].sell_amount - confirmed_amount; //미체결 판매 수량 = 판매 수량 - 체결 수량
                 await stockModel.updateBuy([unconfirmed_buy_amount, matchTrade[0].order_buy_idx]);  //구매 주문 체결량 update
-                await stockModel.updateBuyUserMoney(confirmed_amount*matchTrade[0].order_price, matchTrade[0].buy_user_id);  //유저가 구매시 보유 자산 감소
                 let existedBuyUserstock = await stockModel.getBuyUserStock(matchTrade[0].buy_user_id, matchTrade[0].stock_code); //유저가 주식을 이미 보유하고 있는지 확인
                 if(existedBuyUserstock.length) 
                     await stockModel.updateBuyUserStock([confirmed_amount,confirmed_amount*matchTrade[0].order_price, matchTrade[0].buy_user_id, matchTrade[0].stock_code]); //이미 존재시 변경
                 else
                     await stockModel.insertBuyUserStock(matchTrade[0].buy_user_id, matchTrade[0].stock_code, matchTrade[0].buy_amount, confirmed_amount*matchTrade[0].order_price); //없을 시 추가
 
-                //유저가 구매시 보유 주식수 추가;
-                await stockModel.updateSell([unconfirmed_sell_amount, matchTrade[0].order_sell_idx]); //판매 주문 체결량 update
-                await stockModel.updateSellUserMoney(confirmed_amount*matchTrade[0].order_price, matchTrade[0].sell_user_id);  //유저가 판매시 보유 자산 증가
-                let existedSellUserstock = await stockModel.getSellUserStock(matchTrade[0].sell_user_id, matchTrade[0].stock_code); //주식 보유 수 가져옴
-
-
-                if(existedSellUserstock[0].hold_amount == matchTrade[0].sell_amount) 
-                    await stockModel.deleteSellUserStock(matchTrade[0].sell_user_id, matchTrade[0].stock_code); //보유 수와 판매 수가 같으면 삭제
-                else
-                    await stockModel.updateSellUserStock(confirmed_amount, existedSellUserstock[0].all_price_bought,
-                         existedSellUserstock[0].hold_amount, matchTrade[0].sell_user_id, matchTrade[0].stock_code ); //다르면 변경    
                 
-                //유저가 판매시 보유 주식수 감소
+                await stockModel.updateSell([unconfirmed_sell_amount, matchTrade[0].order_sell_idx]); //판매 주문 체결량 update
+                await stockModel.updateSellUserMoney(confirmed_amount*matchTrade[0].order_price, matchTrade[0].sell_user_id);  //유저가 판매시 보유 자산 증가              
+                
                 await stockModel.tradeConfirmed([matchTrade[0].order_buy_idx, matchTrade[0].order_sell_idx, confirmed_amount]); //체결된 주문 insert
             }   
             else
